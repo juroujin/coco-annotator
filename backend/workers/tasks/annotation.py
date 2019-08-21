536ef7f16ab80f21f5f3a1d85f5718757deb58e9
tonouchi510 <tonouchi27@gmail.com>
2019-08-21 18:34:58 +0900

    Update annotation task

--- backend/workers/tasks/annotation.py
+++ backend/workers/tasks/annotation.py
@@ -10,58 +10,85 @@ from ..socket import create_socket
 import requests
 from PIL import Image
 
-keypoints_order = [0, 15, 14, 17, 16, 5, 2, 6, 3, 7, 4, 11, 8, 12, 9, 13, 10]
-
 
 @shared_task
-def pre_annotation(task_id, image_id):
+def pre_annotation(task_id, dataset_id):
 
     task = TaskModel.objects.get(id=task_id)
-    image = ImageModel.objects.get(id=image_id)
+    dataset = DatasetModel.objects.get(id=dataset_id)
 
     task.update(status="PROGRESS")
     socket = create_socket()
 
-    path = image.path
-    task.info(f"Pre annotation {path}")
+    directory = dataset.directory
+    toplevel = list(os.listdir(directory))
+    task.info(f"Pre annotation {directory}")
 
-    im = Image.open(path).convert('RGB')
-    response = requests.post(
-        "http://localhost:5000/api/model/openpose",
-        { "image": im })
+    count = 0
+    for root, dirs, files in os.walk(directory):
 
-    data = response.json()
-    coco = data["coco"]
-    images = coco["images"]
-    categories = coco["categories"]
-    annotations = coco["annotations"]
+        try:
+            youarehere = toplevel.index(root.split('/')[-1])
+            progress = int(((youarehere)/len(toplevel))*100)
+            task.set_progress(progress, socket=socket)
+        except:
+            pass
 
-    if len(images) == 0 or len(categories) == 0 or len(annotations) == 0:
-        return
+        if root.split('/')[-1].startswith('.'):
+            continue
 
-    indexedCategories = []
-    for c in categories:
-        indexedCategories[c.id] = c
+        for file in files:
+            path = os.path.join(root, file)
 
-    for annotation in annotations:
-        keypoints = annotation["keyopints"]
-        segments = annotation["segmentation"]
-        category = indexedCategories[annotation["category_id"]]
+            if path.endswith(ImageModel.PATTERN):
+                db_image = ImageModel.objects(path=path).first()
 
-        if len(keypoints) == 0 and len(segments) == 0:
-            return
+                if db_image is None:
+                    continue
 
-        category = category["category"]
+                try:
+                    im = Image.open(path).convert("RGB")
+                    response = requests.post(
+                        "http://localhost:5000/api/model/openpose",
+                        { "image": im })
 
-        requests.post(
-            "http://localhost:5000/api/annotation",
-            { "image_id": image_id,
-              "category_id": category["id"],
-              "segmentation": segments,
-              "keypoints": keypoints
-            })
+                    data = response.json()
+                    coco = data["coco"]
+                    images = coco["images"]
+                    categories = coco["categories"]
+                    annotations = coco["annotations"]
 
-    task.info(f"Annotated {path}")
+                    if len(images) == 0 or len(categories) == 0 or len(annotations) == 0:
+                        return
+
+                    indexedCategories = []
+                    for c in categories:
+                        indexedCategories[c.id] = c
+
+                    for annotation in annotations:
+                        keypoints = annotation["keyopints"]
+                        segments = annotation["segmentation"]
+                        category = indexedCategories[annotation["category_id"]]
+
+                        if len(keypoints) == 0 and len(segments) == 0:
+                            return
+
+                        category = category["category"]
+
+                        requests.post(
+                            "http://localhost:5000/api/annotation",
+                            { "image_id": db_image["id"],
+                              "category_id": category["id"],
+                              "segmentation": segments,
+                              "keypoints": keypoints
+                            })
+
+                    count += 1
+                    task.info(f"Pre annotate file: {path}")
+                except:
+                    task.warning(f"Could not read {path}")
+
+    task.info(f"Pre annotated {count} new image(s)")
     task.set_progress(100, socket=socket)
 
 __all__ = ["pre_annotation"]
